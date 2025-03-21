51f09353ee0dcdcd1887f52e0648ec0338361d49
JuanJosePradaContreras <180313522+JuanJosePradaContreras@users.noreply.github.com>
2025-03-21 12:53:05 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function validar() {
+    a = "({[]})";
+
+    if(a === a)
+        return "la expresion es equilibrada"
+    else(a !== a)
+        return "la expresion no es equilibrada"
+}
+
+console.log(validar())
